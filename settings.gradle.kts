e95942b57e0631e01ce18f1bac9234ccb6dce2fe
Filipe Sousa <filipe@ipb.pt>
2023-03-17 17:11:16 +0000

    update plugins and artifacts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ pluginManagement {
   plugins {
     id("com.gradle.enterprise") version "3.12.4"
     id("com.github.ben-manes.versions") version "0.46.0"
-    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("com.github.johnrengelman.shadow") version "8.1.0"
     id("nl.littlerobots.version-catalog-update") version "0.8.0"
     id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.7"
   }
